194bcbea574f991e0748d586bc7faaba03ccc677
Teras <teras@amonia.de>
2023-01-06 23:47:40 +0100

    Load SubScenes directly

---
+++ GFTool.Core/Flatbuffers/TR/Scene/Components/SubScene.cs
@@ -0,0 +1,18 @@
+﻿using FlatSharp.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFTool.Core.Flatbuffers.TR.Scene.Components
+{
+    [FlatBufferTable]
+    public class SubScene
+    {
+        [FlatBufferItem(0)]
+        public string Filepath { get; set; }
+        [FlatBufferItem(1)]
+        public int unk1 { get; set; }
+    }
+}
